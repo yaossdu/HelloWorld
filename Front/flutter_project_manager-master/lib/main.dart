39bcfadbe143b165a31198f793207574ba700d74
yaossdu <810847975@qq.com>
2023-06-26 21:01:53 +0800

    在idea中，第一次使用git commit

--- Front/flutter_project_manager-master/lib/main.dart
+++ Front/flutter_project_manager-master/lib/main.dart
@@ -20,6 +20,7 @@ void init(){
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
